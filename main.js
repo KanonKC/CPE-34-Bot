793deab41c1592017d209fb537e836321f44420c
KanonKC <kanonkce@gmail.com>
2021-06-08 15:04:22 +0700

    Update main.js

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ Client.on("message",async (message)=>{
 
         case "auth":
             if(message.channel.id != "851365772210077737"){break}
-            var id = require("./id")
+            var id = require("./id").id
             console.log(id)
             if(id.includes(arg[1])){
                 message.member.roles.add(Role.member)
